3e5952fd44948596d76949b81a4b8dd9aeda5072
martin-king <martin-king@users.noreply.github.com>
2020-01-14 14:52:17 +0100

    Update cdo_seasmeandjfmean_pdsi.py

--- cdo_seasmeandjfmean_pdsi.py
+++ cdo_seasmeandjfmean_pdsi.py
@@ -14,6 +14,9 @@ for file in filelist:
  print "cdo selmon,11 PDSI_Thornthwaite_b.e11.BLMTRC5CN.f19_g16."+att_in[4]+".cam.h0_seasmean.nc PDSI_Thornthwaite_b.e11.BLMTRC5CN.f19_g16."+att_in[4]+".cam.h0.085011-200511_sonmean.nc"
  print "cdo ymonmean PDSI_Thornthwaite_b.e11.BLMTRC5CN.f19_g16."+att_in[4]+".cam.h0.085011-200511_sonmean.nc PDSI_Thornthwaite_b.e11.BLMTRC5CN.f19_g16."+att_in[4]+".cam.h0.085011-200511_sonmean_ymonmean.nc"
 
+# print "cdo selyear,1900/2004 b.e11.BLMTRC5CN.f19_g16."+att_in[4]+".cam.h0.TS.084912-200412_djfmean.nc b.e11.BLMTRC5CN.f19_g16."+att_in[4]+".cam.h0.TS.084912-200412_djfmean_1899-2003.nc"
+# print "cdo ymonmean b.e11.BLMTRC5CN.f19_g16."+att_in[4]+".cam.h0.TS.084912-200412_djfmean_1899-2003.nc b.e11.BLMTRC5CN.f19_g16."+att_in[4]+".cam.h0.TS.084912-200412_djfmean_1899-2003_ymonmean.nc "
+  
 #ctl is not really needed  print "cp PDSI_Thornthwaite_b.e11.BLMTRC5CN.f19_g16.001.cam.h0_085008-200508_jjamean_ymonmean.ctl"+" PDSI_Thornthwaite_b.e11.BLMTRC5CN.f19_g16."+att_in[4]+".cam.h0.084912-200412_mammean_ymonmean.ctl" 
 #  print 'sed /"DSET"/s/"001"/"'+att_in[4]+'"/'+" PDSI_Thornthwaite_b.e11.BLMTRC5CN.f19_g16."+att_in[4]+".cam.h0.084912-200412_djfmean_ymonmean.ctl > ruba.ctl"
 #  print 'cp ruba.ctl '+" PDSI_Thornthwaite_b.e11.BLMTRC5CN.f19_g16."+att_in[4]+".cam.h0.084912-200412_jjamean_ymonmean.ctl"
